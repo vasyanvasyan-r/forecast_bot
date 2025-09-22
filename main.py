8f3ddb53c56c45fa27edb3715b54905668765fb8
Vasyan <74531162+vasyanvasyan-r@users.noreply.github.com>
2025-09-22 21:34:52 +0300

    Версия 0.2.7 Матч против Ниццы

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from handlers import start, auth, forecast, notifications
 from keyboards.menu import reboot_menu
 from utils.storage import reboot_notifications, authorized_users
 
-test = True
+test = False
 sendmessage = True
 if not test:
     bot = Bot(token=TOKEN)
